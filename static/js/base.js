27dcf2d30a48c81990d010433302f55efdbdf84e
Kharay33455 <shakibkhan276337@gmail.com>
2025-01-19 14:41:18 +0100

    Changed home page to make animation run immediately

--- static/js/base.js
+++ static/js/base.js
@@ -3,6 +3,66 @@ const width = window.screen.width;
 const height = window.screen.height;
 
 
+// run animation as soon as possible
+// Only loop 50 times to save browser resources in case of inactive user
+
+// words to loop in animation
+const words = ['Developer.', 'Engineer.', 'Programmer.', 'Enthusiast.']
+let loopCount = 0;
+
+// recursice function to erase word till there's no letter left
+//afterwards, call write word with the next viable count and increment the loop counter to track loop
+const eraseWord = (index_of_words, i) => {
+    const wordErased = i;
+    const word = words[index_of_words];
+    const title = document.getElementById('title');
+    setTimeout(() => {
+        title.innerHTML = (word.slice(0, word.length - i));
+        i++
+        if (i < word.length + 1) {
+
+            eraseWord(index_of_words, i);
+        }
+        else {
+            loopCount++;
+            if (loopCount < 50) {
+                index_of_words < 3 ? writeWord(index_of_words + 1, 0) : writeWord(0, 0);
+            }
+            else {
+                title.innerHTML = 'Developer.'
+            }
+
+        }
+    }, 100);
+
+}
+
+
+// write words function
+// recursive function to write words till there's none left. If left, erase
+const writeWord = (index_of_words, i) => {
+    let word = words[index_of_words]
+    const title = document.getElementById('title');
+    let count = i || 0;
+
+    title.innerHTML = word.slice(0, i)
+
+    count++;
+    setTimeout(() => {
+        if (count < word.length + 1) {
+            writeWord(index_of_words, count);
+        }
+        else {
+            eraseWord(index_of_words, 0);
+        }
+    }, 200)
+
+}
+
+
+// erase words function
+
+
 const alertOfRender = () => {
     alert("Note: Most of my projects are hosted on free services, which occasionally put sites to sleep to conserve power. As a result, it may take up to 50 seconds for the server to wake up and load the site or show an error incase of pythonanywhere. If the site isn't loading properly, please try reloading the page. Thank you for your understanding.")
 }
@@ -33,6 +93,9 @@ const showRepertoire = () => {
 
 // make sure window has loaded before trying to render DOM
 window.onload = function () {
+    // run first immeidatly dom loads.
+    writeWord(0, 0);
+
     //background image setting
     const backgroundImg = document.getElementById('back');
     backgroundImg.style.width = width + 'px';
@@ -59,66 +122,6 @@ window.onload = function () {
     pfp.style.height = val + 'px';
     pfp.style.borderRadius = val + 'px';
 
-
-    // run animation last after all other resources have loaded
-    // Only loop 50 times to save browser resources in case of inactive user
-
-    // words to loop in animation
-    const words = ['Developer.', 'Engineer.', 'Programmer.', 'Enthusiast.']
-    let loopCount = 0;
-
-    // recursice function to erase word till there's no letter left
-    //afterwards, call write word with the next viable count and increment the loop counter to track loop
-    const eraseWord = (index_of_words, i) => {
-        const wordErased = i;
-        const word = words[index_of_words];
-        const title = document.getElementById('title');
-        setTimeout(() => {
-            title.innerHTML = (word.slice(0, word.length - i));
-            i++
-            if (i < word.length + 1) {
-
-                eraseWord(index_of_words, i);
-            }
-            else {
-                loopCount++;
-                if (loopCount < 50) {
-                    index_of_words < 3 ? writeWord(index_of_words + 1, 0) : writeWord(0, 0);
-                }
-                else {
-                    title.innerHTML = 'Developer.'
-                }
-
-            }
-        }, 100);
-
-    }
-
-
-    // write words function
-    // recursive function to write words till there's none left. If left, erase
-    const writeWord = (index_of_words, i) => {
-        let word = words[index_of_words]
-        const title = document.getElementById('title');
-        let count = i || 0;
-
-        title.innerHTML = word.slice(0, i)
-
-        count++;
-        setTimeout(() => {
-            if (count < word.length + 1) {
-                writeWord(index_of_words, count);
-            }
-            else {
-                eraseWord(index_of_words, 0);
-            }
-        }, 200)
-
-    }
-
-    writeWord(0, 0);
-
-    // erase words function
 
 }
 
